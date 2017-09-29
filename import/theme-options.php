66e99a65c5957eea41e96a0fefa9c1d0128625c6
Ryan B. Abcede <ryan@syntactics.local>
2017-09-29 14:13:52 +0800

    imports

---
+++ import/theme-options.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Initialize the custom theme options.
+ */
+add_action( 'init', 'custom_theme_options' );
+
+/**
+ * Build the custom settings & update OptionTree.
+ */
+function custom_theme_options() {
+  
+  /* OptionTree is not loaded yet, or this is not an admin request */
+  if ( ! function_exists( 'ot_settings_id' ) || ! is_admin() )
+    return false;
+    
+  /**
+   * Get a copy of the saved settings array. 
+   */
+  $saved_settings = get_option( ot_settings_id(), array() );
+  
+  /**
+   * Custom settings array that will eventually be 
+   * passes to the OptionTree Settings API Class.
+   */
+  $custom_settings = array( 
+    'contextual_help' => array( 
+      'sidebar'       => ''
+    ),
+    'sections'        => array( 
+      array(
+        'id'          => 'general',
+        'title'       => 'General'
+      )
+    ),
+    'settings'        => array( 
+      array(
+        'id'          => 'logo',
+        'label'       => 'Logo',
+        'desc'        => 'Description for the sample text field.',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'fb',
+        'label'       => 'fb',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'tw',
+        'label'       => 'tw',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'dr',
+        'label'       => 'dr',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'be',
+        'label'       => 'be',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'gp',
+        'label'       => 'gp',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'rs',
+        'label'       => 'rs',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      )
+    )
+  );
+  
+  /* allow settings to be filtered before saving */
+  $custom_settings = apply_filters( ot_settings_id() . '_args', $custom_settings );
+  
+  /* settings are not the same update the DB */
+  if ( $saved_settings !== $custom_settings ) {
+    update_option( ot_settings_id(), $custom_settings ); 
+  }
+  
+  /* Lets OptionTree know the UI Builder is being overridden */
+  global $ot_has_custom_theme_options;
+  $ot_has_custom_theme_options = true;
+  
+}
